f3db00824a2a476cf2bc65f0ed606b8a8199aac2
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2022-04-29 01:38:08 +1200

    Got a basic demo working

--- src/timeslot.js
+++ src/timeslot.js
@@ -1,5 +1,6 @@
+// W3school strat to draggable div
 // Make the DIV element draggable:
-dragElement(document.getElementById("mydiv"));
+// dragElement(document.getElementById("mydiv"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -44,4 +45,83 @@ function dragElement(elmnt) {
 
 
 
+// This part is my own code 
+const divPage = document.getElementById("page");
+const createTime_button = document.getElementById("createTime");
+const inputTime_input = document.getElementById("inputTime");
 
+// radio buttons
+const radioBreak = document.getElementById("radioBreak");
+const radioWork = document.getElementById("radioWork");
+const radioOther = document.getElementById("radioOther");
+
+/**
+ * The function just adds an evenlistener to the input for 
+ * making 'timeslots'
+ */
+function timeSlotInput() {
+    // when button is clicked
+    createTime_button.addEventListener("click", () => {
+        let type = "";
+        // Lazy method
+        if (radioBreak.checked) {
+            type = "slotBreak";
+        }
+        else if (radioWork.checked) {
+            type = "slotWork";
+        }
+        else {
+            type = "slotOther";
+        }
+        text = inputTime_input.value;
+        inputTime_input.value = "";
+        addTimeSlot(text, type);
+    })
+    // when input is entered
+    inputTime_input.addEventListener("keypress", e => {
+        if (e.key == "Enter") {
+            let type = "";
+            // Lazy method
+            if (radioBreak.checked) {
+                type = "slotBreak";
+            }
+            else if (radioWork.checked) {
+                type = "slotWork";
+            }
+            else {
+                type = "slotOther";
+            }
+            text = inputTime_input.value;
+            inputTime_input.value = "";
+            addTimeSlot(text, type);
+        }
+    })
+}
+
+
+/**
+ * Creates a draggable div to the DOM
+ * @param {String} text represents the description of the timeslot
+ * @param {String} type represents the css class it is part for color coding
+ */
+function addTimeSlot(text, type) {
+    const header = document.createElement("div");
+    // too lazy to use an actual way to create unique id
+    // to make sure the drag element doesn't break
+    // may refactor, to remove this 
+    const randomNum = Math.floor(Math.random() * 10000)
+    header.className = "slotHeader";
+    header.id = "t" + randomNum +"header";
+
+    const timeslot = document.createElement("div");
+    timeslot.className = "timeslot " + type;
+    timeslot.id = "t" + randomNum;
+
+    timeslot.appendChild(header);
+    timeslot.append(text);
+    divPage.appendChild(timeslot);
+    dragElement(timeslot);
+}
+
+
+timeSlotInput();
